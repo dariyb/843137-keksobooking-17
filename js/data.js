8493d32572c6cf37e4c2eb1774398f9534c350de
dariy <gomzykovad@gmail.com>
2019-06-26 18:18:54 +1000

    problem

--- js/data.js
+++ js/data.js
@@ -1,15 +1,11 @@
 'use strict';
 (function () {
   window.util = {
-    TYPE_OF_OFFER: ['palace', 'flat', 'house', 'bungalo'],
-    minXLocation: 0,
-    maxXLocation: 1200,
-    minYLocation: 130,
-    maxYLocation: 630,
     marker: 8,
     pinSize: 65,
     activeMap: document.querySelector('.map__pin--main'),
     adForm: document.querySelector('.ad-form'),
-    pinAddress: document.querySelector('input[name=address]')
+    pinAddress: document.querySelector('input[name=address]'),
+    pinData: window.backend.load()
   };
 })();
--- js/pin.js
+++ js/pin.js
@@ -3,76 +3,27 @@ window.pinModule = (function () {
   var blockElements = document.querySelector('.map__pins');
   var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
-  var getAvatarNumber = function () {
-    var avatarsList = [];
-    for (var i = 1; i < window.util.marker + 1; i++) {
-      var avatars = 'img/avatars/user0' + i + '.png';
-      avatarsList.push(avatars);
+  var createNewPin = function (data) {
+    var pinMarker = pinTemplate.cloneNode(true);
+    pinMarker.style.left = (data.location.x + (window.util.pinSize / 2)) + 'px';
+    if (data.location.y >= (window.util.maxYLocation - window.util.pinSize)) {
+      pinMarker.style.top = (data.location.y - window.util.pinSize) + 'px';
+    } else {
+      pinMarker.style.top = (data.location.y + window.util.pinSize) + 'px';
     }
-    return avatarsList;
-  };
+    pinMarker.querySelector('img').src = data.author.avatar;
+    pinMarker.querySelector('img').alt = data.offer.type;
 
-  var getPlace = function () {
-    for (var i = 0; i < window.util.TYPE_OF_OFFER.length; i++) {
-      var randomIndex = Math.floor(Math.random() * window.util.TYPE_OF_OFFER.length);
-    }
-    return randomIndex;
+    return pinMarker;
   };
-
-  var getLocation = function (min, max) {
-    return Math.floor(Math.random() * ((max + 1) - min) + min);
-  };
-
-  var getAds = function () {
-    var advert = [];
-    var list = getAvatarNumber();
-    for (var i = 0; i < window.util.marker; i++) {
-      advert.push(
-          {
-            author:
-            {
-              avatar: list[i]
-            },
-            offer:
-            {
-              type: window.util.TYPE_OF_OFFER[getPlace()]
-            },
-            location:
-            {
-              x: getLocation(window.util.minXLocation, window.util.maxXLocation),
-              y: getLocation(window.util.minYLocation, window.util.maxYLocation)
-            }
-          }
-      );
-    }
-    return advert;
-  };
-  var advert = getAds();
-
-  var createNewPin = function () {
-    var newPinsList = [];
-    for (var i = 0; i < window.util.marker; i++) {
-      var pinMarker = pinTemplate.cloneNode(true);
-      pinMarker.style.left = (advert[i].location.x + (window.util.pinSize / 2)) + 'px';
-      if (advert[i].location.y >= (window.util.maxYLocation - window.util.pinSize)) {
-        pinMarker.style.top = (advert[i].location.y - window.util.pinSize) + 'px';
-      } else {
-        pinMarker.style.top = (advert[i].location.y + window.util.pinSize) + 'px';
-      }
-      pinMarker.querySelector('img').src = advert[i].author.avatar;
-      pinMarker.querySelector('img').alt = advert[i].offer.type;
-      newPinsList.push(pinMarker);
-    }
-    return newPinsList;
-  };
-  var newPinsList = createNewPin();
-  var insertPins = function () {
+  var insertPins = function (pin) {
     var fragment = document.createDocumentFragment();
-    for (var i = 0; i < advert.length; i++) {
-      fragment.appendChild(newPinsList[i]);
+    for (var i = 0; i < pin.length; i++) {
+      fragment.appendChild(createNewPin(pin[i]));
     }
     blockElements.appendChild(fragment);
   };
+  window.backend.load(insertPins);
   return {
     insertPins: insertPins
   };
