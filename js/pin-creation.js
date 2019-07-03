82f518d8f470291c54ee08b41502820e361ef575
dariy <gomzykovad@gmail.com>
2019-07-04 00:17:09 +1000

    pin location done

--- js/pin-creation.js
+++ js/pin-creation.js
@@ -4,7 +4,7 @@ window.creationModule = (function () {
 
   var createNewPin = function (data) {
     var pinMarker = pinTemplate.cloneNode(true);
-    pinMarker.style.left = (data.location.x + (window.util.pinWidth / 2)) + 'px';
+    pinMarker.style.left = (data.location.x - (window.util.pinWidth / 2)) + 'px';
     pinMarker.style.top = (data.location.y - window.util.pinHeigth) + 'px';
     pinMarker.querySelector('img').src = data.author.avatar;
     pinMarker.querySelector('img').alt = data.offer.title;
